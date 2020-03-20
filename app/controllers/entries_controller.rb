a2d79158998c4e30896aef46efef48f70de97923
Max Arellano <maxfrda@gmail.com>
2020-03-20 21:01:31 +0100

    you can now submit content via the p tag

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,21 +1,24 @@
 require 'date'
 class EntriesController < ApplicationController
   def home
-    @entry = Entry.new(entry_params)
+    @entry = Entry.new
   end
 
   def index
     @entry = Entry.all
+
   end
   def create
     @entry = Entry.new(entry_params)
     @entry.date = Date.today.strftime
+    @entry.save
+    redirect_to entries_path
   end
 
   private
 
   def entry_params
-    params.permit(:date, :body)
+    params.require(:entry).permit(:date, :body)
   end
 
 end
