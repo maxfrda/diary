dbf7f6557ac0f1000ccb3a26d13239aedfa91ec4
Max Arellano <maxfrda@gmail.com>
2020-03-24 21:37:38 +0100

    added devise

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,6 +2,7 @@ require 'date'
 class EntriesController < ApplicationController
   def home
     @entry = Entry.new
+    @user = current_user
   end
 
   def index
@@ -11,8 +12,14 @@ class EntriesController < ApplicationController
   def create
     @entry = Entry.new(entry_params)
     @entry.date = Date.today.strftime
-    @entry.save
-    redirect_to entries_path
+    @entry.user = current_user
+
+    if
+      @entry.save
+      redirect_to entries_path
+    else
+      redirect_to new_user_registration_path
+  end
   end
 
   private
